4e5d66ad3f2d1c9b5479539ce8ab3b78f9a87a3a
Thiago <thesars.info@gmail.com>
2024-03-06 23:18:41 -0300

    Inicio de Projeto

---
+++ src/main/java/com/example/petanimalslost/PetanimalslostApplication.java
@@ -0,0 +1,13 @@
+package com.example.petanimalslost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetanimalslostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetanimalslostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petanimalslost/PetanimalslostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petanimalslost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetanimalslostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
